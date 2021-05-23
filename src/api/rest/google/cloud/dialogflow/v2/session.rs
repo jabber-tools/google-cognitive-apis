3e6b6385b6ec3555aa410eed1328f94fdfbe2a75
adambezecny <Adam.Bezecny@dhl.com>
2021-05-23 22:31:47 +0200

    tts, dialogflow: strutas for JSON deser added

--- src/api/rest/google/cloud/dialogflow/v2/session.rs
+++ src/api/rest/google/cloud/dialogflow/v2/session.rs
@@ -1,11 +1,25 @@
 use serde::Deserialize;
+use serde_json::Value as JsonData;
 
-/// simplified dialogflow response
-/// deserializes only what we need
+/// Draft of DetectIntentResponse. Full specification here:
+/// https://cloud.google.com/dialogflow/es/docs/reference/rest/v2/DetectIntentResponse
+/// Currently QueryResult is not fully mapped into structs. We map only what we need.
 #[derive(Debug, Deserialize)]
-pub struct DialogFlowResponse {
-    #[serde(rename = "queryResult")]
+pub struct DetectIntentResponse {
+    #[serde(rename = "responseId")]
+    pub response_id: String,
+
+    #[serde(rename = "queryResult", skip_serializing_if = "Option::is_none")]
     pub query_result: Option<QueryResult>,
+
+    #[serde(rename = "webhookStatus", skip_serializing_if = "Option::is_none")]
+    pub webhook_status: Option<Status>,
+
+    #[serde(rename = "outputAudio", skip_serializing_if = "Option::is_none")]
+    pub output_audio: Option<String>,
+
+    #[serde(rename = "outputAudioConfig", skip_serializing_if = "Option::is_none")]
+    pub output_audio_config: Option<OutputAudioConfig>,
 }
 
 #[derive(Debug, Deserialize)]
@@ -17,4 +31,62 @@ pub struct QueryResult {
 #[derive(Debug, Deserialize)]
 pub struct DiagnosticInfo {
     pub end_conversation: bool,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct OutputAudioConfig {
+    #[serde(rename = "audioEncoding")]
+    pub audio_encoding: i32,
+
+    #[serde(rename = "sampleRateHertz")]
+    pub sample_rate_hertz: i32,
+
+    #[serde(
+        rename = "synthesizeSpeechConfig",
+        skip_serializing_if = "Option::is_none"
+    )]
+    pub synthesize_speech_config: Option<SynthesizeSpeechConfig>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Status {
+    code: i32,
+    message: String,
+    details: Vec<JsonData>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct SynthesizeSpeechConfig {
+    #[serde(rename = "speakingRate")]
+    pub speaking_rate: f64,
+
+    pub pitch: f64,
+
+    #[serde(rename = "volumeGainDb")]
+    pub volume_gain_db: f64,
+
+    #[serde(rename = "effectsProfileId")]
+    pub effects_profile_id: Vec<String>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub voice: Option<VoiceSelectionParams>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct VoiceSelectionParams {
+    name: String,
+
+    #[serde(rename = "ssmlGender")]
+    ssml_gender: i32,
+}
+
+// TBD: use this for VoiceSelectionParams.ssml_gender
+// need to find real i32 values, google doc does not specify them
+#[derive(Debug, Deserialize)]
+#[allow(non_camel_case_types)]
+pub enum SsmlVoiceGender {
+    SSML_VOICE_GENDER_UNSPECIFIED = 0,
+    SSML_VOICE_GENDER_MALE = 1,
+    SSML_VOICE_GENDER_FEMALE = 2,
+    SSML_VOICE_GENDER_NEUTRAL = 3,
 }
--- src/api/rest/google/cloud/texttospeech/v1.rs
+++ src/api/rest/google/cloud/texttospeech/v1.rs
@@ -1,14 +1,68 @@
-///
-/// Not yet implemented!
-///
-#[derive(Debug)]
-pub struct TTSRequest {
-    /// text or ssml to synthetize
-    pub text: String,
+use crate::api::grpc::google::cloud::texttospeech::v1::{
+    AudioConfig as GrpcAudioConfig, VoiceSelectionParams as GrpcVoiceSelectionParams,
+};
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+pub struct VoiceSelectionParams {
+    #[serde(rename = "languageCode")]
+    pub language_code: String,
+
+    pub name: String,
+
+    #[serde(rename = "ssmlGender")]
+    pub ssml_gender: i32,
+}
+impl From<GrpcVoiceSelectionParams> for VoiceSelectionParams {
+    fn from(grpc_vsp: GrpcVoiceSelectionParams) -> Self {
+        VoiceSelectionParams {
+            language_code: grpc_vsp.language_code,
+            name: grpc_vsp.name,
+            ssml_gender: grpc_vsp.ssml_gender,
+        }
+    }
 }
 
-#[derive(Debug)]
-pub struct TTSResponse {
-    /// synthetized audio data
-    pub audio_base64: String,
+// TBD: use this for VoiceSelectionParams.ssml_gender
+// need to find real i32 values, google doc does not specify them
+#[derive(Debug, Deserialize)]
+#[allow(non_camel_case_types)]
+pub enum SsmlVoiceGender {
+    SSML_VOICE_GENDER_UNSPECIFIED = 0,
+    MALE = 1,
+    FEMALE = 2,
+    NEUTRAL = 3,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct AudioConfig {
+    #[serde(rename = "audioEncoding")]
+    pub audio_encoding: i32,
+
+    #[serde(rename = "speakingRate")]
+    pub speaking_rate: f64,
+
+    pub pitch: f64,
+
+    #[serde(rename = "volumeGainDb")]
+    pub volume_gain_db: f64,
+
+    #[serde(rename = "sampleRateHertz")]
+    pub sample_rate_hertz: i32,
+
+    #[serde(rename = "effectsProfileId")]
+    pub effects_profile_id: Vec<String>,
+}
+
+impl From<GrpcAudioConfig> for AudioConfig {
+    fn from(grpc_audio_cfg: GrpcAudioConfig) -> Self {
+        AudioConfig {
+            audio_encoding: grpc_audio_cfg.audio_encoding,
+            speaking_rate: grpc_audio_cfg.speaking_rate,
+            pitch: grpc_audio_cfg.pitch,
+            volume_gain_db: grpc_audio_cfg.volume_gain_db,
+            sample_rate_hertz: grpc_audio_cfg.sample_rate_hertz,
+            effects_profile_id: grpc_audio_cfg.effects_profile_id,
+        }
+    }
 }
