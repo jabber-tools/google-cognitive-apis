5e21c0b295ffc67c9a271e4fb1ef1995c62d1d42
adambezecny <Adam.Bezecny@dhl.com>
2021-05-23 11:39:36 +0200

    CI finetuning

--- src/common.rs
+++ src/common.rs
@@ -55,6 +55,7 @@ pub async fn new_grpc_channel(
 
 /// Returns google token (String value) from
 /// Google Cloud Platform project JSON credentials (provided as String).
+#[allow(clippy::rc_buffer)]
 pub fn get_token(google_credentials: impl AsRef<str>) -> Result<Arc<String>> {
     let token = Builder::new().json(google_credentials).build()?;
     let token_header_val: Arc<String> = token.header_value()?;
